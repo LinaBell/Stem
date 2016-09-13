e5a6c19f7bd536105781039ecf37c78c1592ba97
Melaina Bell <linabell@mail.com>
2016-09-12 23:37:03 -0700

    login / who are you fix

--- assets/scripts/whoareyou.jsx
+++ assets/scripts/whoareyou.jsx
@@ -1,30 +1,46 @@
 var WhoAreYou = React.createClass({
-	handleClick: function(id) {
+	handleClick: function(type) {
 		var self = this;
 
 		stemApi.createAccount({
 			request: {
-				AccountType: id
+				AccountType: type
 			},
 			success: function(response) {
 				console.log('success!');
 				console.log(JSON.stringify(response, null, 2));
-				store.dispatch((dispatch) => {
-					dispatch({
-						type: 'UpdateUserRecord',
-						data: { userInfo: response }
-			 		})
-					dispatch({
-						type: 'GoToPage',
-						data: { currentPage: 0 }
-			 		})
-		 		})
+				if (response.accountType == 'Artist') {
+					page = 6;
+				} else if (response.accountType == 'Creator') {
+					page = 10;
+				} else if (response.accountType == 'Admin') {
+					page = 20;
+				}
+				self.updateLoginStatus(true, response, page);
 			},
 			error: function() {
 				console.error(JSON.stringify(response, null, 2));
 			}
 		});
 	},
+
+    updateLoginStatus: function(isLoggedIn, userInfo, currentPage) {
+		store.dispatch((dispatch) => {
+			dispatch({
+				type: 'UpdateLoginStatus',
+				data: {
+					isLoggedIn: isLoggedIn,
+					userInfo: userInfo
+				}
+			});
+
+	    	dispatch({
+	        	type: 'GoToPage',
+	        	data: {currentPage: currentPage}
+	    	});
+		})
+    },
+
 
 	render: function() {
 		var self = this;
@@ -45,11 +61,11 @@ var WhoAreYou = React.createClass({
 						</div>
 						<div className="row pad-r-md pad-l-md">
 							<div className="col-sm-4">
-								<img onClick={this.handleClick.bind(self, 1)} className="btn-circle" width="200px" src="assets/images/WhoRU_Creator.png" />
+								<img onClick={this.handleClick.bind(self, 'Creator')} className="btn-circle" width="200px" src="assets/images/WhoRU_Creator.png" />
 								<h4 className="pad-b-md spacer">Creator</h4>
 							</div>
 							<div className="col-sm-4">
-								<img onClick={this.handleClick.bind(self, 2)} className="btn-circle" width="200px" src="assets/images/WhoRU_Musician.png" />
+								<img onClick={this.handleClick.bind(self, 'Artist')} className="btn-circle" width="200px" src="assets/images/WhoRU_Musician.png" />
 								<h4 className="pad-b-md spacer">Musician</h4>
 							</div>
 							<div className="col-sm-4">
