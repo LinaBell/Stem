a35e8f34acc4c0224b077f0b363fdd7d2f36c9c4
Jeremy Miller <jjm340@gmail.com>
2016-10-15 20:17:29 -0700

    Added the player using jwplayer and made it accept a song id, did some cleanup on unneeded files

--- assets/scripts/musicPlayer.jsx
+++ assets/scripts/musicPlayer.jsx
@@ -1,90 +1,56 @@
 var MusicPlayer = React.createClass({
+	player: null,
 	getInitialState: function() {
-		return{
-			audioSource: "http://ejb.github.io/progressor.js/demos/echoplex.mp3",
-			player: undefined,
-			isPlaying: false
+		return {
+			canPlay: false
 		};
 	},
 
-	componentDidMount: function() {
-		var wavesurfer = WaveSurfer.create({
-		    container: '#waveform',
-		    height: 110,
-		    barWidth: 2,
-		    cursorWidth: 2,
-		    cursorColor: "rgba(82, 84, 95, 0.5)",
-		    scrollParent: true,
-		    hideScrollbar: true,
-		    waveColor: "#00D8D7",
-		    progressColor: "#c9c9c9",
-		});
-		this.setState({player: wavesurfer});
-		wavesurfer.load(this.state.audioSource);
+    componentWillReceiveProps(nextProps) {
+
+    	var isUnitialized = (this.player === null);
+
+    	if (isUnitialized || this.props.songId !== nextProps.songId) {
+	    	stemApi.streamSong({
+	    		id: nextProps.songId
+	    	})
+	    	.then((res) => {
+
+	    		if (isUnitialized) {
+		    		var player = jwplayer('music-player').setup({
+				    	file: res.url,
+				    	// A height of 40 puts this in audio mode
+				    	height: 40,
+				    	width: 500,
+				    	type: 'mp3'
+					});
+
+					this.player = player;
+		    	} else {
+		    		this.player.load(res.url);
+		    	} 
+
+	    		this.setState({
+	    			canPlay: true
+	    		})
+	    	})
+    	}
     },
 
-    playPause: function() {
-    	var wavesurfer = this.state.player;
-    	wavesurfer.playPause();
-
-        if (this.state.isPlaying == false) {
-        	$('.icon-pause').css("display", "inline");
-        	$('.icon-play').css("display", "none");
-            this.setState({isPlaying: true});
-        } else {
-        	$('.icon-play').css("display", "inline");
-        	$('.icon-pause').css("display", "none");
-            this.setState({isPlaying: false});
-        }
+    componentWillUnmount() {
+    	this.player.remove();
     },
-
-    // playPauseClick: function() {
-        // var audio = document.getElementsByTagName('audio')[0];
-
-        // if (audio.paused == true) {
-        //     audio.play();
-        //     $('.pause').css("display","inline");
-        //     $('.play').css("display","none");
-        // } else {
-        //     audio.pause();
-        //     $('.pause').css("display","none");
-        //     $('.play').css("display","inline");
-        // }
-    // },
-
-    // muteClick: function() {
-    //     var audio = document.getElementsByTagName('audio')[0];
-
-    //     if (audio.muted == false) {
-    //         audio.muted = true;
-    //         $('#muted').css("display","inline");
-    //         $('#unmuted').css("display","none");
-    //     } else {
-    //         audio.muted = false;
-    //         $('#muted').css("display","none");
-    //         $('#unmuted').css("display","inline");
-    //     }
-    // },
-
-    // volumeChange: function() {
-    //     var audio = document.getElementsByTagName('audio')[0];
-    //     audio.volume = volumeBar.value;
-    // },
 
 	render: function() {
 		
 		return(
 			<div className="promo-song-info">
-                <a onClick={this.playPause} className="promo-artist-img-a mar-r-lg">
-					<img className="promo-artist-img" src="https://a4-images.myspacecdn.com/images03/33/588cae99266a4ae2a9c49c909b02781c/600x600.jpg"/>
-					<span className="play-pause icon-pause"></span>
-					<span className="play-pause icon-play"></span>
-                </a>
                 <div className="right-side-content">
 					<h2 className="mar-t-sm">InMemory - Never forget</h2>
 					<div id="waveform" className="mar-t-sm mar-b-sm"></div>
 					<h3 className="display-inlb">Follow this artist:</h3>
-					<FollowThisArtistLinks/>
+					<FollowThisArtistLinks />
+					<div id="music-player"></div>
 				</div>
 			</div>
 		);
--- assets/scripts/promoLandingPage/promoLandingTopSection.jsx
+++ assets/scripts/promoLandingPage/promoLandingTopSection.jsx
@@ -10,7 +10,7 @@ var PromoLandingTopSection = React.createClass({
 		return(
 			<div className="promo-landing-top-section">
 				<div className="promo-background header-banner" style={bannerImageStyle}></div>
-				<MusicPlayer />
+				<MusicPlayer songId="33" />
 				<div className="promo-share-this">
 					<p className="display-inlb">Share this: </p>
 					<div className="promo-share-link display-inlb pad-box-md mar-l-md mar-r-md mar-b-sm">https://www.thmatc.co/inmemoryrocksmysocks/266262426622</div>
